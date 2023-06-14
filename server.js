8124eedd8840e6dd1438a9f0efddae10eb3b1d81
dejvi <dejviepadejvi@gmail.com>
2023-06-14 09:52:28 +0200

    changes

--- server.js
+++ server.js
@@ -9,15 +9,32 @@ dotenv.config();
 const connectDB = require("./config/db");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 app.use(express.json());
+const path = require("path");
 
 app.use(cors());
 connectDB();
-app.get("/", (req, res) => {
-  res.send("Hello NODE API");
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// ------------- deployment -----------
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client-side/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../client-side", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Hello NODE API");
+  });
+}
+
+// -------------- deployment -------------
 
 app.use(notFound);
 app.use(errorHandler);
